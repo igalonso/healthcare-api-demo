b7a4ef3a35d6b0cb7ff5d9cdde9bc85f93d95154
igalonso <igalonso@gmail.com>
2022-04-11 15:37:09 +0200

    redefined reid and demo consent

--- site/main.js
+++ site/main.js
@@ -18,31 +18,10 @@ function show_image(src, width, height, alt,where) {
     document.getElementById(where).appendChild(img);
 }
 
-function syntaxHighlight(json) {
-    if (typeof json != 'string') {
-         json = JSON.stringify(json, undefined, 2);
-    }
-    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-        var cls = 'number';
-        if (/^"/.test(match)) {
-            if (/:$/.test(match)) {
-                cls = 'key';
-            } else {
-                cls = 'string';
-            }
-        } else if (/true|false/.test(match)) {
-            cls = 'boolean';
-        } else if (/null/.test(match)) {
-            cls = 'null';
-        }
-        return '<span class="' + cls + '">' + match + '</span>';
-    });
-}
-
 $(document).ready(function(){
+    var jsonViewer = new JSONViewer();
     $('#researcher-show-list').click(function(){
-        var url = "http://127.0.0.1:5000/demo";
+        var url = "http://127.0.0.1:5000/demo/datasets";
         
         fetch(url,{
             method: "GET",
@@ -59,6 +38,7 @@ $(document).ready(function(){
             ul.replaceChildren();
             data.forEach(element => {
                 var li = document.createElement("li");
+                li.className = "researcher-list-li";
                 li.appendChild(document.createTextNode(element));
                 ul.appendChild(li); 
             });
@@ -69,7 +49,7 @@ $(document).ready(function(){
         });
     })
     $('#researcher-show-sample-image').click(function(){
-        var url = "http://127.0.0.1:5000/demo/demo-dataset/datastores/datastore-ds-raw/sample-image";
+        var url = "http://127.0.0.1:5000/demo/datasets/demo-dataset/datastores/datastore-ds-raw/sample-image";
         fetch(url,{
             method: "GET",
             mode: "cors"
@@ -80,6 +60,9 @@ $(document).ready(function(){
             }
         })
         .then(data => {
+            document.getElementById("researcher-sample-image").style.display = "block";
+            document.getElementById("json_sample_image_tags").style.display = "none";
+            document.getElementById("researcher-sample-image").replaceChildren();
             show_image(url,281,213,data,"researcher-sample-image");
         })
         .catch(function(error) {
@@ -88,7 +71,7 @@ $(document).ready(function(){
         
     })
     $('#researcher-sample-image-show-tags').click(function(){
-        var url = "http://127.0.0.1:5000/demo/demo-dataset/datastores/datastore-ds-raw/sample-image?onlytags=true";
+        var url = "http://127.0.0.1:5000/demo/datasets/demo-dataset/datastores/datastore-ds-raw/sample-image?onlytags=true";
         fetch(url,{
             method: "GET",
             mode: "cors"
@@ -98,10 +81,14 @@ $(document).ready(function(){
                 throw new Error(response.error)
             }
             response.text().then(function(result){
-                var div = document.getElementById("researcher-sample-image");
-                var p = document.createElement("p");
-                p.appendChild(document.createTextNode(result));
-                div.appendChild(p); 
+                document.getElementById("researcher-sample-image").style.display = "none";
+                document.getElementById("json_sample_image_tags").style.display = "block";
+                var div = document.getElementById("json_sample_image_tags");
+                div.replaceChildren();
+                div.appendChild(jsonViewer.getContainer());
+                maxLvl = 7;
+                colAt = 7;
+                jsonViewer.showJSON(JSON.parse(result), maxLvl, colAt); 
             })
         })
         .catch(function(error) {
@@ -109,7 +96,7 @@ $(document).ready(function(){
         });
     })
     $('#researcher-show-deid-image').click(function(){
-        var url = "http://127.0.0.1:5000/demo/demo-dataset/datastores/datastore-ds-raw/sample-image/deid";
+        var url = "http://127.0.0.1:5000/demo/datasets/demo-dataset/datastores/datastore-ds-raw/sample-image/deid";
         fetch(url,{
             method: "GET",
             mode: "cors"
@@ -120,11 +107,140 @@ $(document).ready(function(){
             }
         })
         .then(data => {
-            console.log(data);
+            document.getElementById("researcher-sample-image-deid").style.display = "block";
+            document.getElementById("json_sample_deid_image_tags").style.display = "none";
+            document.getElementById("researcher-sample-image-deid").replaceChildren();
             show_image(url,281,213,data,"researcher-sample-image-deid");
         })
         .catch(function(error) {
             alert(error);
         });
     })
+    $('#researcher-show-deid-image-tags').click(function(){
+        var url = "http://127.0.0.1:5000/demo/datasets/demo-dataset/datastores/datastore-ds-raw/sample-image/deid?onlytags=true";
+        fetch(url,{
+            method: "GET",
+            mode: "cors"
+        })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(response.error)
+            }
+            response.text().then(function(result){
+                document.getElementById("researcher-sample-image-deid").style.display = "none";
+                document.getElementById("json_sample_deid_image_tags").style.display = "block";
+                var div = document.getElementById("json_sample_deid_image_tags");
+                div.replaceChildren();
+                div.appendChild(jsonViewer.getContainer());
+                maxLvl = 7;
+                colAt = 7;
+                jsonViewer.showJSON(JSON.parse(result), maxLvl, colAt); 
+            })
+        })
+        .catch(function(error) {
+            alert(error);
+        });
+    })
+
+    $('#patient-list-mappings').click(function(){
+        var url = "http://127.0.0.1:5000//demo/consents/demo-dataset/datastores/consent-ds/usermappings";
+        fetch(url,{
+            method: "GET",
+            mode: "cors"
+        })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(response.error)
+            }
+            response.text().then(function(result){
+                document.getElementById("json_patient_tags").style.display = "block";
+                var div = document.getElementById("json_patient_tags");
+                div.replaceChildren();
+                div.appendChild(jsonViewer.getContainer());
+                maxLvl = 7;
+                colAt = 7;
+                jsonViewer.showJSON(JSON.parse(result), maxLvl, colAt); 
+            })
+        })
+        .catch(function(error) {
+            alert(error);
+        });
+    })
+
+    $('#patient-list-consent').click(function(){
+        var url = "http://127.0.0.1:5000//demo/consents/demo-dataset";
+        fetch(url,{
+            method: "GET",
+            mode: "cors"
+        })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(response.error)
+            }
+            response.text().then(function(result){
+                document.getElementById("json_patient_tags").style.display = "block";
+                var div = document.getElementById("json_patient_tags");
+                div.replaceChildren();
+                div.appendChild(jsonViewer.getContainer());
+                maxLvl = 7;
+                colAt = 7;
+                jsonViewer.showJSON(JSON.parse(result), maxLvl, colAt); 
+            })
+        })
+        .catch(function(error) {
+            alert(error);
+        });
+    })
+
+    $('#doctor-request-consent').click(function(){
+        var url = "http://127.0.0.1:5000//demo/consents/demo-dataset/datastores/consent-ds/create";
+        fetch(url,{
+            method: "GET",
+            mode: "cors"
+        })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(response.error)
+            }
+            response.text().then(function(result){
+                document.getElementById("json_doctor_tags").style.display = "block";
+                var div = document.getElementById("json_doctor_tags");
+                div.replaceChildren();
+                div.appendChild(jsonViewer.getContainer());
+                maxLvl = 7;
+                colAt = 7;
+                jsonViewer.showJSON(JSON.parse(result), maxLvl, colAt); 
+            })
+        })
+        .catch(function(error) {
+            alert(error);
+        });
+    })
+
+    $('#patient-grant-consent').click(function(){
+        var url = "http://127.0.0.1:5000//demo/consents/demo-dataset/datastores/consent-ds/consents/activate";
+        fetch(url,{
+            method: "GET",
+            mode: "cors"
+        })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(response.error)
+            }
+            response.text().then(function(result){
+                document.getElementById("json_patient_tags").style.display = "block";
+                var div = document.getElementById("json_patient_tags");
+                div.replaceChildren();
+                div.appendChild(jsonViewer.getContainer());
+                maxLvl = 7;
+                colAt = 7;
+                jsonViewer.showJSON(JSON.parse(result), maxLvl, colAt); 
+            })
+        })
+        .catch(function(error) {
+            alert(error);
+        });
+    })
+
+    
 })
