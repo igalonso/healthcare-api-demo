0acc265e5b957feb8e38f5a71189ae60c07bac3f
igalonso <igalonso@gmail.com>
2022-07-18 18:01:15 +0200

    adding document ai to NLP

--- install.sh
+++ install.sh
@@ -32,8 +32,8 @@ gcloud config set project $GCP_PROJECT
 echo "2 ----> Creating the bucket"
 gsutil mb -c standard -l $REGION gs://$BUCKET
 gsutil cp 1-1-2.dcm gs://$BUCKET
-echo "3 ----> Enabling Healthcare API"
-gcloud services enable healthcare.googleapis.com 
+echo "3 ----> Enabling Healthcare API and Document AI and translate API"
+gcloud services enable healthcare.googleapis.com documentai.googleapis.com translate.googleapis.com
 echo "4 ----> Creating Healthcare Datasets"
 
 gcloud healthcare datasets create $HC_DATASET --location $REGION
@@ -77,12 +77,25 @@ sed -i "s/<GCP_PROJECT>/$GCP_PROJECT/" configuration.ini
 #if mac
 sed -i '' 's/<GCP_PROJECT>/'$GCP_PROJECT'/g' configuration.ini
 
+echo "8. Creating a Document AI Procesor"
+export GCLOUDTOKEN=$(gcloud auth print-access-token)
 
+export DOC_AI_PROC=$(curl -H "Authorization: Bearer $GCLOUDTOKEN" -H "Content-Type: application/json" "https://eu-documentai.googleapis.com/v1/projects/$GCP_PROJECT/locations/eu/processors" -X POST -d '{  "name": "procesor-hc-api","displayName": "procesor-hc-api", "type": "OCR_PROCESSOR"}' | jq -r '.name')
+DOC_AI_PROC="projects/468594267618/locations/eu/processors/9814d0788509993b"
+FIELDS=$(echo $DOC_AI_PROC | tr "/" "\n")
+
+for field in $FIELDS
+do
+    OCR_NAME=$field
+done
+#echo $OCR_NAME
+
+#if not mac
+sed -i "s/<OCR_ID>/$OCR_NAME/" configuration.ini
+#if mac
+sed -i '' 's/<OCR_ID>/'$OCR_NAME'/g' configuration.ini
 echo "8 ----> Finished!"
 echo "      run:"
 echo "          sh run.sh"
 echo "      to execute the demo"
-
-
-
 
